4ae1667d7fa57c693934c617313d014eb8bb59ad
Lana <virsen.sv@gmail.com>
2025-10-16 23:10:33 +0200

    Add Lists exercise with Spectre.Console menu and Book list features

---
+++ ListsExercise/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace Collections_exercises.ListsExercise
+{
+    public class Book
+    {
+        public string Title;
+        public string Author;
+        public int YearPublished;
+
+        // Include constructor so it's possible to create and initialize a Book object in a single statement.
+        public Book(string title, string author, int yearPublished)
+        {
+            Title = title;
+            Author = author;
+            YearPublished = yearPublished;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿namespace Collections_exercises
+﻿using Collections_exercises.ArraysExercise;
+using Collections_exercises.ListsExercise;
+
+namespace Collections_exercises
 {
     internal class Program
     {
         static void Main()
         {
-            Arrays.Run();
-            //Lists.Run();
+            //Arrays.Run();
+            Lists.Run();
         }
     }
 }
